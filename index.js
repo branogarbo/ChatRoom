9cb875f6eea4c34943b48aed8611381cbea2a403
Brian Longmore <brianl.ext@gmail.com>
2020-06-06 20:28:27 -1000

    got it workin, chat box expands when overflowing for some reason

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 const path = require('path');
 const port = 3000;
 
@@ -6,3 +7,15 @@ const app = express();
 const server = app.listen(port, console.log(`listening on port ${port}`));
 
 app.use(express.static(path.join(__dirname,'public')));
+
+// socket setup
+
+let io = socket(server);
+
+io.on('connection', socket=>{
+   console.log('made socket connection. socket id:',socket.id);
+
+   socket.on('chat', data=>{
+      io.sockets.emit('chat', data);
+   });
+});
---
+++ public/script.js
@@ -0,0 +1,27 @@
+let socket = io.connect('http://localhost:3000');
+
+let qs = sel => document.querySelector(sel);
+
+// emit events
+
+qs('#sendButton').onclick = () => {
+   socket.emit('chat', {
+      name: qs('#name').value,
+      message: qs('#message').value
+   });
+
+   qs('#message').value = "";
+};
+
+socket.on('chat', data=>{
+   qs('#chat').insertAdjacentHTML('beforeend', `
+      <div>
+         <div><b>${data.name}:</b> ${data.message}</div>
+         <hr />
+      </div>
+   `);
+
+   qs('#chat > div:last-of-type').scrollIntoView({
+      behavior: 'smooth'
+   });
+});
